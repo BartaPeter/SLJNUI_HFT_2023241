f2c3be2261881ca297164cb0ad30234811806967
BartaPeter <148630438+BartaPeter@users.noreply.github.com>
2023-10-22 21:10:16 +0200

    Update Program.cs

--- SLJNUI_HFT_2023241.Client/Program.cs
+++ SLJNUI_HFT_2023241.Client/Program.cs
@@ -6,7 +6,7 @@ namespace SLJNUI_HFT_2023241.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("cigany");
+            Console.WriteLine("bajnok");
         }
     }
 }
