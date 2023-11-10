7357245f0acd60a0b1a32426b8113b2623779574
BartaPeter <lombotom17@gmail.com>
2023-11-10 15:15:24 +0100

    CourierLogic created

---
+++ SLJNUI_HFT_2023241.Logic/CourierLogic.cs
@@ -0,0 +1,45 @@
+﻿using SLJNUI_HFT_2023241.Models;
+using SLJNUI_HFT_2023241.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLJNUI_HFT_2023241.Logic
+{
+    public class CourierLogic : IRestaurantLogic, ICourierLogic
+    {
+        IRepository<Courier> repository;
+
+        public CourierLogic(IRepository<Courier> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Courier item)
+        {
+            this.repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public Courier Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public IQueryable<Courier> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(Courier item)
+        {
+            this.repository.Update(item);
+        }
+    }
+}
---
+++ SLJNUI_HFT_2023241.Logic/ICourierLogic.cs
@@ -0,0 +1,14 @@
+﻿using SLJNUI_HFT_2023241.Models;
+using System.Linq;
+
+namespace SLJNUI_HFT_2023241.Logic
+{
+    public interface ICourierLogic
+    {
+        void Create(Courier item);
+        void Delete(int id);
+        Courier Read(int id);
+        IQueryable<Courier> ReadAll();
+        void Update(Courier item);
+    }
+}
