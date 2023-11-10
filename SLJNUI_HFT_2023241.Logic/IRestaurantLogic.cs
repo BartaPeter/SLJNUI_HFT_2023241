ac9073273158405488e83a13d3be960087cf7eae
BartaPeter <lombotom17@gmail.com>
2023-11-10 15:08:41 +0100

    Interface created

---
+++ SLJNUI_HFT_2023241.Logic/IRestaurantLogic.cs
@@ -0,0 +1,14 @@
+﻿using SLJNUI_HFT_2023241.Models;
+using System.Linq;
+
+namespace SLJNUI_HFT_2023241.Logic
+{
+    public interface IRestaurantLogic
+    {
+        void Create(Restaurant item);
+        void Delete(int id);
+        Restaurant Read(int id);
+        IQueryable<Restaurant> ReadAll();
+        void Update(Restaurant item);
+    }
+}
--- SLJNUI_HFT_2023241.Logic/RestaurantLogic.cs
+++ SLJNUI_HFT_2023241.Logic/RestaurantLogic.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace SLJNUI_HFT_2023241.Logic
 {
-    public class RestaurantLogic
+    public class RestaurantLogic : IRestaurantLogic
     {
         IRepository<Restaurant> repository;
 
