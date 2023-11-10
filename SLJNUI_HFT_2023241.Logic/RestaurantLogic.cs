ec2b3d831a9bd741457f8ebd33dd4d173fda988d
BartaPeter <lombotom17@gmail.com>
2023-11-10 15:07:02 +0100

    RestaurantLogic uploaded

--- SLJNUI_HFT_2023241.Logic/RestaurantLogic.cs
+++ SLJNUI_HFT_2023241.Logic/RestaurantLogic.cs
@@ -1,8 +1,42 @@
-﻿using System;
+﻿using SLJNUI_HFT_2023241.Models;
+using SLJNUI_HFT_2023241.Repository;
+using System;
+using System.Linq;
 
 namespace SLJNUI_HFT_2023241.Logic
 {
-    public class Class1
+    public class RestaurantLogic
     {
+        IRepository<Restaurant> repository;
+
+        public RestaurantLogic(IRepository<Restaurant> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Restaurant item)
+        {
+            this.repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public Restaurant Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public IQueryable<Restaurant> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(Restaurant item)
+        {
+            this.repository.Update(item);
+        }
     }
 }
