3c12e468cd70a414ecb6a43f32f2363f83ecaa24
BartaPeter <lombotom17@gmail.com>
2023-11-10 15:13:39 +0100

    FoodLogic created

---
+++ SLJNUI_HFT_2023241.Logic/FoodLogic.cs
@@ -0,0 +1,45 @@
+﻿using SLJNUI_HFT_2023241.Models;
+using SLJNUI_HFT_2023241.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLJNUI_HFT_2023241.Logic
+{
+    public class FoodLogic : IFoodLogic, IFoodLogic
+    {
+        IRepository<Food> repository;
+
+        public FoodLogic(IRepository<Food> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Food item)
+        {
+            this.repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public Food Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public IQueryable<Food> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(Food item)
+        {
+            this.repository.Update(item);
+        }
+    }
+}
---
+++ SLJNUI_HFT_2023241.Logic/IFoodLogic.cs
@@ -0,0 +1,14 @@
+﻿using SLJNUI_HFT_2023241.Models;
+using System.Linq;
+
+namespace SLJNUI_HFT_2023241.Logic
+{
+    public interface IFoodLogic
+    {
+        void Create(Food item);
+        void Delete(int id);
+        Food Read(int id);
+        IQueryable<Food> ReadAll();
+        void Update(Food item);
+    }
+}
