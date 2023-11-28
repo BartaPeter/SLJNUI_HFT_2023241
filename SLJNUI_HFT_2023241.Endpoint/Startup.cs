0958404d2b18469f92e9b527386e7d16fa066b31
BartaPeter <lombotom17@gmail.com>
2023-11-28 11:45:11 +0100

    Controller.cs added

--- SLJNUI_HFT_2023241.Endpoint/Startup.cs
+++ SLJNUI_HFT_2023241.Endpoint/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SLJNUI_HFT_2023241.Logic;
+using SLJNUI_HFT_2023241.Models;
 using SLJNUI_HFT_2023241.Repository;
 using System;
 using System.Collections.Generic;
@@ -17,7 +19,7 @@ using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
 
-namespace MovieDbApp.Endpoint
+namespace SLJUNI_HFT_2023241.Endpoint
 {
     public class Startup
     {
@@ -27,37 +29,30 @@ namespace MovieDbApp.Endpoint
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<MovieDbContext>();
+            services.AddTransient<RestaurantDbContext>();
+            services.AddTransient<IRepository<Restaurant>, RestaurantRepository>();
+            services.AddTransient<IRepository<Courier>, CourierRepository>();
+            services.AddTransient<IRepository<Food>, FoodRepository>();
 
-            services.AddTransient<IRepository<Movie>, MovieRepository>();
-            services.AddTransient<IRepository<Role>, RoleRepository>();
-            services.AddTransient<IRepository<Actor>, ActorRepository>();
-            services.AddTransient<IRepository<Director>, DirectorRepository>();
-
-            services.AddTransient<IMovieLogic, MovieLogic>();
-            services.AddTransient<IRoleLogic, RoleLogic>();
-            services.AddTransient<IActorLogic, ActorLogic>();
-            services.AddTransient<IDirectorLogic, DirectorLogic>();
+            services.AddTransient<IRestaurantLogic, RestaurantLogic>();
+            services.AddTransient<ICourierLogic, CourierLogic>();
+            services.AddTransient<IFoodLogic, FoodLogic>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MovieDbApp.Endpoint", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SLJNUI_HFT_202341.Endpoint", Version = "v1" });
             });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieDbApp.Endpoint v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SLJNUI_HFT_202341.Endpoint v1"));
             }
 
             app.UseExceptionHandler(c => c.Run(async context =>
